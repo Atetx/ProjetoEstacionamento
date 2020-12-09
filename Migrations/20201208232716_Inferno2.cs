0536b949c33a9e57b2283d151f4e3203bddd2767
Atetx <tessari.leo@gmail.com>
2020-12-08 21:07:07 -0300

    Commit UsuarioCNPJ e API

---
+++ Migrations/20201208232716_Inferno2.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EstacionamentoWeb.Migrations
+{
+    public partial class Inferno2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Capital_social",
+                table: "Usuario");
+
+            migrationBuilder.DropColumn(
+                name: "Cnpj",
+                table: "Usuario");
+
+            migrationBuilder.DropColumn(
+                name: "Fantasia",
+                table: "Usuario");
+
+            migrationBuilder.DropColumn(
+                name: "Situacao",
+                table: "Usuario");
+
+            migrationBuilder.DropColumn(
+                name: "Tipo",
+                table: "Usuario");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UsuarioCNPJId",
+                table: "Estacionamento",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "UsuarioCNPJ",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fantasia = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Capital_social = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Situacao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cnpj = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsuarioCNPJ", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Estacionamento_UsuarioCNPJId",
+                table: "Estacionamento",
+                column: "UsuarioCNPJId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Estacionamento_UsuarioCNPJ_UsuarioCNPJId",
+                table: "Estacionamento",
+                column: "UsuarioCNPJId",
+                principalTable: "UsuarioCNPJ",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Estacionamento_UsuarioCNPJ_UsuarioCNPJId",
+                table: "Estacionamento");
+
+            migrationBuilder.DropTable(
+                name: "UsuarioCNPJ");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Estacionamento_UsuarioCNPJId",
+                table: "Estacionamento");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioCNPJId",
+                table: "Estacionamento");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Capital_social",
+                table: "Usuario",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Cnpj",
+                table: "Usuario",
+                type: "nvarchar(14)",
+                maxLength: 14,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Fantasia",
+                table: "Usuario",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Situacao",
+                table: "Usuario",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tipo",
+                table: "Usuario",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
---
+++ Migrations/20201208233932_Inferno3.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EstacionamentoWeb.Migrations
+{
+    public partial class Inferno3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Estacionamento_Usuario_UsuarioId",
+                table: "Estacionamento");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Estacionamento_UsuarioCNPJ_UsuarioCNPJId",
+                table: "Estacionamento");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Estacionamento_UsuarioId",
+                table: "Estacionamento");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UsuarioCNPJ",
+                table: "UsuarioCNPJ");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "Estacionamento");
+
+            migrationBuilder.RenameTable(
+                name: "UsuarioCNPJ",
+                newName: "UsuariosCnpj");
+
+            migrationBuilder.RenameColumn(
+                name: "UsuarioCNPJId",
+                table: "Estacionamento",
+                newName: "UsuarioCnpjId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Estacionamento_UsuarioCNPJId",
+                table: "Estacionamento",
+                newName: "IX_Estacionamento_UsuarioCnpjId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UsuarioCnpjId",
+                table: "Estacionamento",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UsuariosCnpj",
+                table: "UsuariosCnpj",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Estacionamento_UsuariosCnpj_UsuarioCnpjId",
+                table: "Estacionamento",
+                column: "UsuarioCnpjId",
+                principalTable: "UsuariosCnpj",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Estacionamento_UsuariosCnpj_UsuarioCnpjId",
+                table: "Estacionamento");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UsuariosCnpj",
+                table: "UsuariosCnpj");
+
+            migrationBuilder.RenameTable(
+                name: "UsuariosCnpj",
+                newName: "UsuarioCNPJ");
+
+            migrationBuilder.RenameColumn(
+                name: "UsuarioCnpjId",
+                table: "Estacionamento",
+                newName: "UsuarioCNPJId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Estacionamento_UsuarioCnpjId",
+                table: "Estacionamento",
+                newName: "IX_Estacionamento_UsuarioCNPJId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UsuarioCNPJId",
+                table: "Estacionamento",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UsuarioId",
+                table: "Estacionamento",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UsuarioCNPJ",
+                table: "UsuarioCNPJ",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Estacionamento_UsuarioId",
+                table: "Estacionamento",
+                column: "UsuarioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Estacionamento_Usuario_UsuarioId",
+                table: "Estacionamento",
+                column: "UsuarioId",
+                principalTable: "Usuario",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Estacionamento_UsuarioCNPJ_UsuarioCNPJId",
+                table: "Estacionamento",
+                column: "UsuarioCNPJId",
+                principalTable: "UsuarioCNPJ",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
